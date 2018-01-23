9e94f7b92d6899fd94d8362bafef59d892bf3a05
linqining <linqining@outlook.com>
2018-01-23 14:26:11 +0800

    add create function

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,22 @@
+class PostsController < ApplicationController
+     before_action :authenticate_user!, :only => [:create, :destroy]
+
+   def create
+     @post = Post.new(post_params)
+     @post.user = current_user
+     @post.save
+   end
+
+   def destroy
+     @post = current_user.posts.find(params[:id]) # 只能删除自己的贴文
+     @post.destroy
+   end
+   def index
+     @posts = Post.order("id DESC").all    # 新贴文放前面
+   end
+   protected
+
+   def post_params
+     params.require(:post).permit(:content)
+   end
+end
